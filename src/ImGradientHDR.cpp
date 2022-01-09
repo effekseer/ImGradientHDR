6aa18b08d989af4dbe9342df58dce2aee48dd584
durswd <swda.durl@gmail.com>
2022-01-09 23:59:58 +0900

    wip

--- src/ImGradientHDR.cpp
+++ src/ImGradientHDR.cpp
@@ -1,6 +1,8 @@
 #include "ImGradientHDR.h"
 
 #include <imgui.h>
+#include <vector>
+#include <algorithm>
 
 bool ImGradientHDR(ImGradientHDRState& state)
 {
@@ -8,11 +10,123 @@ bool ImGradientHDR(ImGradientHDRState& state)
 
 	auto drawList = ImGui::GetWindowDrawList();
 
-	ImGui::InvisibleButton("Bar", { 100,100 });
+	const int height = 20;
+
+	ImGui::InvisibleButton("Bar", { 100,height });
 
 	drawList->AddRectFilled(ImVec2(originPos.x - 2, originPos.y - 2),
-		ImVec2(originPos.x + 100 + 2, originPos.y + 100 + 2),
+		ImVec2(originPos.x + 100 + 2, originPos.y + height + 2),
 		IM_COL32(100, 100, 100, 255));
+
+	if (state.ColorCount == 0 && state.AlphaCount == 0)
+	{
+
+	}
+	else
+	{
+		std::vector<float> xkeys;
+		xkeys.reserve(16);
+
+		for (int32_t i = 0; i < state.ColorCount; i++)
+		{
+			xkeys.emplace_back(state.ColorPositions[i]);
+		}
+
+		for (int32_t i = 0; i < state.AlphaCount; i++)
+		{
+			xkeys.emplace_back(state.AlphaPositions[i]);
+		}
+
+		xkeys.emplace_back(0.0f);
+		xkeys.emplace_back(1.0f);
+
+		auto result = std::unique(xkeys.begin(), xkeys.end());
+		xkeys.erase(result, xkeys.end());
+
+		std::sort(xkeys.begin(), xkeys.end());
+
+		auto getColor = [&](float x)
+		{
+			if (state.ColorCount == 0)
+			{
+				return std::array<float, 3>{1.0f, 1.0f, 1.0f};
+			}
+
+			if (x < state.ColorPositions[0])
+			{
+				return state.Colors[0];
+			}
+
+			if (state.ColorPositions[state.ColorCount - 1] <= x)
+			{
+				return state.Colors[state.ColorCount - 1];
+			}
+
+			for (int i = 0; i < state.ColorCount - 1; i++)
+			{
+				if (state.ColorPositions[i] <= x && x < state.ColorPositions[i + 1])
+				{
+					const auto area = state.ColorPositions[i + 1] - state.ColorPositions[i];
+					const auto alpha = (x - state.ColorPositions[i]) / area;
+					const auto r = state.Colors[i + 1][0] * alpha + state.Colors[i][0] * (1.0f - alpha);
+					const auto g = state.Colors[i + 1][1] * alpha + state.Colors[i][1] * (1.0f - alpha);
+					const auto b = state.Colors[i + 1][2] * alpha + state.Colors[i][2] * (1.0f - alpha);
+					return std::array<float, 3>{ r, g, b };
+				}
+			}
+
+			return std::array<float, 3>{1.0f, 1.0f, 1.0f};
+		};
+
+		auto getAlpha = [&](float x)
+		{
+			if (state.AlphaCount == 0)
+			{
+				return 1.0f;
+			}
+
+			if (x < state.AlphaPositions[0])
+			{
+				return 1.0f;
+			}
+
+			if (state.AlphaPositions[state.ColorCount - 1] <= x)
+			{
+				return state.AlphaPositions[state.ColorCount - 1];
+			}
+
+			for (int i = 0; i < state.AlphaCount - 1; i++)
+			{
+				if (state.AlphaPositions[i] <= x && x < state.AlphaPositions[i + 1])
+				{
+					const auto area = state.AlphaPositions[i + 1] - state.AlphaPositions[i];
+					const auto alpha = (x - state.AlphaPositions[i]) / area;
+					return state.AlphaPositions[i + 1] * (1.0f - alpha) + state.AlphaPositions[i] * alpha;
+				}
+			}
+
+			return 1.0f;
+		};
+
+		auto getRgba = [&](float x)
+		{
+			const auto c = getColor(x);
+			return std::array<float, 4>{c[0], c[1], c[2], getAlpha(x)};
+		};
+
+		for (size_t i = 0; i < xkeys.size() - 1; i++)
+		{
+			const auto c1 = getRgba(xkeys[i]);
+			const auto c2 = getRgba(xkeys[i + 1]);
+
+			const auto colorAU32 = ImGui::ColorConvertFloat4ToU32({ c1[0], c1[1], c1[2], c1[3] });
+			const auto colorBU32 = ImGui::ColorConvertFloat4ToU32({ c2[0], c2[1], c2[2], c2[3] });
+
+			drawList->AddRectFilledMultiColor(ImVec2(originPos.x + xkeys[i] * 100, originPos.y),
+				ImVec2(originPos.x + xkeys[i + 1] * 100, originPos.y + height),
+				colorAU32, colorBU32, colorBU32, colorAU32);
+		}
+	}
 
 	return true;
 }
--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,3 @@
-// Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
-// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
-// If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
-// Read online: https://github.com/ocornut/imgui/tree/master/docs
-
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
@@ -12,9 +7,6 @@
 #endif
 #include <GLFW/glfw3.h> // Will drag system OpenGL headers
 
-// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
-// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
-// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
 #if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
 #pragma comment(lib, "legacy_stdio_definitions")
 #endif
@@ -23,6 +15,8 @@ static void glfw_error_callback(int error, const char* description)
 {
 	fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
+
+#include <GLFW/glfw3.h>
 
 #include <ImGradientHDR.h>
 
@@ -56,101 +50,54 @@ int main(int, char**)
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 #endif
 
-	// Create window with graphics context
 	GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
 	if (window == NULL)
 		return 1;
 	glfwMakeContextCurrent(window);
-	glfwSwapInterval(1); // Enable vsync
+	glfwSwapInterval(1);
 
-	// Setup Dear ImGui context
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
-	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
-	// Setup Dear ImGui style
 	ImGui::StyleColorsDark();
-	//ImGui::StyleColorsClassic();
 
-	// Setup Platform/Renderer backends
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
 	ImGui_ImplOpenGL3_Init(glsl_version);
 
-	// Load Fonts
-	// - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-	// - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-	// - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-	// - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-	// - Read 'docs/FONTS.md' for more instructions and details.
-	// - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-	//io.Fonts->AddFontDefault();
-	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-	//io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
-	//ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-	//IM_ASSERT(font != NULL);
-
-	// Our state
 	bool show_another_window = false;
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 	ImGradientHDRState state;
+	state.ColorCount = 2;
+	state.ColorPositions[0] = 0.2f;
+	state.ColorPositions[1] = 0.4f;
+	state.Colors[0] = { 1.0f, 0.0f, 1.0f };
+	state.Colors[1] = { 0.0f, 1.0f, 0.0f };
 
 	// Main loop
 	while (!glfwWindowShouldClose(window))
 	{
-		// Poll and handle events (inputs, window resize, etc.)
-		// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-		// - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-		// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-		// Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
 		glfwPollEvents();
 
-		// Start the Dear ImGui frame
+
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
 
-
-		// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
 		{
 			static float f = 0.0f;
 			static int counter = 0;
 
-			ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+			ImGui::Begin("Hello, world!");
 
 			ImGradientHDR(state);
 
-			ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-			ImGui::Checkbox("Another Window", &show_another_window);
-
-			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-			if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-				counter++;
-			ImGui::SameLine();
-			ImGui::Text("counter = %d", counter);
-
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			ImGui::Text("End");
 			ImGui::End();
 		}
 
-		// 3. Show another simple window.
-		if (show_another_window)
-		{
-			ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-			ImGui::Text("Hello from another window!");
-			if (ImGui::Button("Close Me"))
-				show_another_window = false;
-			ImGui::End();
-		}
-
-		// Rendering
 		ImGui::Render();
 		int display_w, display_h;
 		glfwGetFramebufferSize(window, &display_w, &display_h);
